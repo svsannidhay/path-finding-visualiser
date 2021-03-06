8d7dc736ec4a82c644d34ee17f3b0292e3255680
svsannidhay <sannidhayvashalsv@gmail.com>
2021-03-06 18:44:16 +0530

    draggable start and end nodes are added

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Node from './Node';
 
+
+
 class Board extends React.Component {
 
   constructor(props) {
@@ -9,33 +11,45 @@ class Board extends React.Component {
       nodes: [],
       startNodeSelected: false,
       destNodeSelected: false,
+      startNodeMoving: false,
+      destNodeMoving: false
     };
   }
 
-  componentDidMount() {
+  createBoard = () => {
     const nodes = [];
-    for (let i=0; i<15; i++) {
+    for (let i=0; i<25; i++) {
       const row = [];
       for (let j=0; j<50; j++) {
         row.push(0);
       }
       nodes.push(row);
     }
-    nodes[7][15] = 1;
-    this.setState({nodes});
+    for (let i=0; i<25; i++) {
+      for (let j=0; j<50; j++) {
+        nodes[i][j] = this.state.nodes[i][j];
+      }
+    }
+    return nodes;
   }
+
+  componentDidMount() {
+    const board = [];
+    for (let i=0; i<25; i++) {
+      const row = [];
+      for (let j=0; j<50; j++) {
+        row.push(0);
+      }
+      board.push(row);
+    }
+    this.setState({nodes: board});
+  }
+
+
 
   onNodeSelect = (rowIndex,colIndex) => {
     console.log(`Node selected ${rowIndex} ${colIndex}`);
-    const newNodes = [];
-    for(let i=0;i<15;i++) {
-      const row = [];
-      for(let j=0;j<50;j++) {
-        row.push(this.state.nodes[i][j]);
-      }
-      newNodes.push(row);
-    }
-    
+    const newNodes = this.createBoard();
     if(this.state.startNodeSelected===false) {
       newNodes[rowIndex][colIndex] = 1;
       document.getElementById(`${rowIndex}+${colIndex}`).classList.add('board__startNode');
@@ -53,8 +67,55 @@ class Board extends React.Component {
       document.getElementById(`${rowIndex}+${colIndex}`).classList.add('board__wallNode');
     }
     this.setState({nodes: newNodes});
-
   }
+
+  onButtonDown = (rowIndex,colIndex) => {
+    console.log(`${rowIndex},${colIndex}`);
+    //If its a start node
+    if(this.state.nodes[rowIndex][colIndex]===1) {
+      const newNodes = this.createBoard();
+      newNodes[rowIndex][colIndex] = 0;
+      document.getElementById(`${rowIndex}+${colIndex}`).classList.remove('board__startNode');
+      this.setState({
+        startNodeMoving: true,
+        nodes: newNodes
+      });
+    } 
+    // If its a destination node
+    if(this.state.nodes[rowIndex][colIndex]===3) {
+      const newNodes = this.createBoard();
+      newNodes[rowIndex][colIndex] = 0;
+      document.getElementById(`${rowIndex}+${colIndex}`).classList.remove('board__destNode');
+      this.setState({
+        destNodeMoving: true,
+        nodes: newNodes
+      });
+    }
+  }
+
+  onButtonUp = (rowIndex,colIndex) => {
+    console.log(`${rowIndex},${colIndex}`);
+    //if Start was moved then place it here
+    if(this.state.nodes[rowIndex][colIndex]!==3&&this.state.startNodeMoving=== true) {
+      const newNodes = this.createBoard();
+      newNodes[rowIndex][colIndex] = 1;
+      document.getElementById(`${rowIndex}+${colIndex}`).classList.add('board__startNode');
+      this.setState({
+        startNodeMoving: false,
+        nodes: newNodes
+      });
+    }
+    //if destination was moved then place it here
+    if(this.state.nodes[rowIndex][colIndex]!==1&&this.state.destNodeMoving=== true) {
+      const newNodes = this.createBoard();
+      newNodes[rowIndex][colIndex] = 3;
+      document.getElementById(`${rowIndex}+${colIndex}`).classList.add('board__destNode');
+      this.setState({
+        destNodeMoving: false,
+        nodes: newNodes
+      });
+    }
+  } 
 
   render() {
     console.log(this.state.nodes);
@@ -63,7 +124,7 @@ class Board extends React.Component {
         {
           this.state.nodes.map((row,i) => {
             return <div className = {`board__row--${i}`} key = {`${i}`}>
-              { row.map((node,j) => <Node key = {`${i}+${j}`}       onNodeSelect = {this.onNodeSelect} nodeVal = {this.state.nodes[i][j]} row = {i} col = {j}></Node>) }
+              { row.map((node,j) => <Node key = {`${i}+${j}`}  onNodeSelect = {this.onNodeSelect} onButtonDown = {this.onButtonDown} onButtonUp = {this.onButtonUp} nodeVal = {this.state.nodes[i][j]} row = {i} col = {j}></Node>) }
             </div>
           })
         }
--- src/components/Node.js
+++ src/components/Node.js
@@ -6,7 +6,17 @@ class Node extends React.Component {
   render () {
     return (
       <div className="board__node">
-        <div onClick = {() => this.props.onNodeSelect(this.props.row,this.props.col)} id = {`${this.props.row}+${this.props.col}`} className = {`board__node--content ${this.props.row}+${this.props.col} board__node--gradient-border`}></div>
+        <div 
+        onClick = {() => this.props.onNodeSelect(this.props.row,this.props.col)} 
+        onMouseDown = {() => this.props.onButtonDown(this.props.row,this.props.col)}
+        onMouseUp = {() => this.props.onButtonUp(this.props.row,this.props.col)}
+        id = {`${this.props.row}+${this.props.col}`} 
+        className = {
+          `board__node--content 
+          ${this.props.row}+${this.props.col} 
+          board__node--gradient-border`
+        }>
+        </div>
       </div>  
     );
   }
