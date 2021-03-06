55a0b9243e2bebbf484d80a0f9800dea94fe712e
svsannidhay <sannidhayvashalsv@gmail.com>
2021-03-06 16:58:44 +0530

    start node, destination node, and walls added

--- src/components/Board.js
+++ src/components/Board.js
@@ -6,7 +6,9 @@ class Board extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      nodes: []
+      nodes: [],
+      startNodeSelected: false,
+      destNodeSelected: false,
     };
   }
 
@@ -19,11 +21,39 @@ class Board extends React.Component {
       }
       nodes.push(row);
     }
+    nodes[7][15] = 1;
     this.setState({nodes});
   }
 
   onNodeSelect = (rowIndex,colIndex) => {
     console.log(`Node selected ${rowIndex} ${colIndex}`);
+    const newNodes = [];
+    for(let i=0;i<15;i++) {
+      const row = [];
+      for(let j=0;j<50;j++) {
+        row.push(this.state.nodes[i][j]);
+      }
+      newNodes.push(row);
+    }
+    
+    if(this.state.startNodeSelected===false) {
+      newNodes[rowIndex][colIndex] = 1;
+      document.getElementById(`${rowIndex}+${colIndex}`).classList.add('board__startNode');
+      this.setState({
+        startNodeSelected: true
+      });
+    } else if (this.state.destNodeSelected===false) {
+      newNodes[rowIndex][colIndex] = 3;
+      document.getElementById(`${rowIndex}+${colIndex}`).classList.add('board__destNode');
+      this.setState ({
+        destNodeSelected: true
+      });
+    } else if (newNodes[rowIndex][colIndex]!==1&&newNodes[rowIndex][colIndex]!==3) {
+      newNodes[rowIndex][colIndex] = 2;
+      document.getElementById(`${rowIndex}+${colIndex}`).classList.add('board__wallNode');
+    }
+    this.setState({nodes: newNodes});
+
   }
 
   render() {
@@ -33,7 +63,7 @@ class Board extends React.Component {
         {
           this.state.nodes.map((row,i) => {
             return <div className = {`board__row--${i}`} key = {`${i}`}>
-              { row.map((node,j) => <Node onNodeSelect = {this.onNodeSelect} key = {`${i}+${j}`} row = {i} col = {j}></Node>) }
+              { row.map((node,j) => <Node key = {`${i}+${j}`}       onNodeSelect = {this.onNodeSelect} nodeVal = {this.state.nodes[i][j]} row = {i} col = {j}></Node>) }
             </div>
           })
         }
--- src/components/Node.js
+++ src/components/Node.js
@@ -1,19 +1,13 @@
 import React from 'react';
 
+
 class Node extends React.Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      rowIndex: this.props.row,
-      colIndex: this.props.col
-    }
-  }
   
   render () {
     return (
-      <div onClick = {() => this.props.onNodeSelect(this.props.row,this.props.col) } className="board__node">
-        <div className = {`board__node--content ${this.state.rowIndex}+${this.state.colIndex} board__node--gradient-border`}></div>
-      </div>
+      <div className="board__node">
+        <div onClick = {() => this.props.onNodeSelect(this.props.row,this.props.col)} id = {`${this.props.row}+${this.props.col}`} className = {`board__node--content ${this.props.row}+${this.props.col} board__node--gradient-border`}></div>
+      </div>  
     );
   }
 };
